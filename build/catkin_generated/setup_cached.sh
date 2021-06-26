df6b957b9b17ce44ad790b343f54aa1ef1e0810c
manu042k <manu042k@gmail.com>
2021-06-26 16:10:38 +0530

    added nav stack

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/manoj/my_bot/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/manoj/my_bot/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/manoj/my_bot/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/manoj/my_bot/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/manoj/my_bot/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/manoj/my_bot/src:$ROS_PACKAGE_PATH"
